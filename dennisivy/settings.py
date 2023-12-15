146437d9d2497b76c5aad51786002ddb89c87a18
hizkiagtg <hizkia.sebastian@ui.ac.id>
2023-12-16 03:46:33 +0700

    Selesai

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hrj48*mobkzo)s6q+2wfqcg=_=@5c38n&sb4_vlp#bb^zyp_pi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
